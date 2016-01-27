8b463a893123524c5d5bba58944d8caa86296afb
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2016-01-27 15:28:35 +0000

    missing dot

--- get_fs_data.sh
+++ get_fs_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp -i ~/.ssh/build.pem ubuntu@test.naturforskaren.se:/tmp/nf-data.tgz srv/data
+scp -i ~/.ssh/build.pem ubuntu@test.naturforskaren.se:/tmp/nf-data.tgz srv/data .
 cd srv/data
 tar xvfz nf-data.tgz --strip-components=2
