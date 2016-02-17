031a824d58a024642fc9f0c8087804b3ff43a8b4
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2016-02-17 16:04:00 +0000

    few changes

--- get_war.sh
+++ get_war.sh
@@ -3,7 +3,8 @@
 #SRC=/media/buffalo/dina-data/naturalist/naturalist.war
 #cp $SRC srv/deployments
 
-SRC=/media/buffalo/dina-data/naturalist/integrated/naturalist-mediaserver.war
+#SRC=/media/buffalo/dina-data/naturalist/integrated/naturalist-mediaserver.war
+SRC=/media/buffalo/dina-data/naturalist/integrated/naturalist-mediaserver-20160216.war
 cp $SRC srv/deployments
 
 SRC=/media/buffalo/dina-data/naturalist/integrated/mediaserver-nf.ear
