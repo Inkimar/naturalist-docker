45785d06e53076ce6ae48685142eee79c9173d74
Ingimar E <inkimar@gmail.com>
2016-09-23 14:25:08 +0200

    not using war from here, packaged in image

--- get_enhanced-naturalist_war.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILE=naturalist_mediaserver_20160920.war
-NEW_FILE=naturalist.war
-SRC=/media/buffalo/dina-data/naturalist/integrated/${FILE}
-DST=wildfly-custom
-test -f $DST/$NEW_FILE || cp $SRC $DST/$NEW_FILE
